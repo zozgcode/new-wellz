ee007fa84828c2f5c99bfd1caddc160d5a267afc
sunshine-web-coder <webcoderdev02@gmail.com>
2023-09-07 05:03:38 +0100

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img className="Logo" src="https://i.imgur.com/gaShZcx.png" alt="logo" />
+      <div className="hamburger">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/userDB.jsx
@@ -0,0 +1,11 @@
+const users = [
+  {
+    id: 1,
+    name: "James Radford",
+    cardName: "James Radford",
+    username: "Jamesradford1",
+    password: "James01",
+    amount: "1,010,000.00",
+  },
+];
+export default users;
